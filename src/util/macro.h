04d112a7d5b1c947c9fb4ea2d042812a7cd850f6
cRTrn13 <cRTrn13@anonymous.org>
2009-10-25 07:04:28 +0000

    wrapped X_FREE() in do..while(0) as per j45's suggestion

--- src/util/macro.h
+++ src/util/macro.h
@@ -8,6 +8,6 @@
 #define MKINT_BE16(X) ( (X)[0] << 8 | (X)[1] )
 #define MKINT_BE24(X) ( (X)[0] << 16 | (X)[1] << 8 | (X)[2] )
 #define MKINT_BE32(X) ( (X)[0] << 24 | (X)[1] << 16 |  (X)[2] << 8 | (X)[3] )
-#define X_FREE(X) { if (X) free(X); X = NULL; }
+#define X_FREE(X) do { if (X) free(X); X = NULL; } while(0)
 
 #endif /* MACRO_H_ */
