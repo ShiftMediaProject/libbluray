078e4b026facc844b7f10031f042d9a731a37277
cRTrn13 <cRTrn13@anonymous.org>
2010-01-06 12:59:58 +0000

    API comments and added bd_get_title_size()

--- src/bluray.h
+++ src/bluray.h
@@ -21,10 +21,13 @@ struct bluray {
     fptr_int libaacs_decrypt_unit;
 };
 
-BLURAY *bd_open(const char* device_path, const char* keyfile_path);
-void bd_close(BLURAY *bd);
-off_t bd_seek(BLURAY *bd, uint64_t pos);
-int bd_read(BLURAY *bd, unsigned char *buf, int len);
-int bd_select_title(BLURAY *bd, uint64_t title);
+BLURAY *bd_open(const char* device_path, const char* keyfile_path); // Init libbluray objs
+void bd_close(BLURAY *bd);                                          // Free libbluray objs
+
+off_t bd_seek(BLURAY *bd, uint64_t pos);                // Seek to pos in currently selected title file
+int bd_read(BLURAY *bd, unsigned char *buf, int len);   // Read from currently selected title file, decrypt if possible
+
+int bd_select_title(BLURAY *bd, uint64_t title);    // Select an m2ts file (title is the file number, e.g. title = 123 will select 00123.m2ts)
+uint64_t bd_get_title_size(BLURAY *bd);             // Returns file size in bytes of currently selected title, 0 in no title selected
 
 #endif /* BLURAY_H_ */
