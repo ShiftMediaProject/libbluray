e7ae4c7413a479e1d9e14cc2c627abf8430ad595
hpi1 <hpi1@anonymous.org>
2010-04-25 11:44:26 +0000

    Added BD_ATTR_PACKED

--- src/util/attributes.h
+++ src/util/attributes.h
@@ -4,9 +4,11 @@
 #if __GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 3 )
 #  define BD_ATTR_FORMAT_PRINTF(format,var) __attribute__((__format__(__printf__,format,var)))
 #  define BD_ATTR_MALLOC                    __attribute__((__malloc__))
+#  define BD_ATTR_PACKED                    __attribute__((packed))
 #else
 #  define BD_ATTR_FORMAT_PRINTF(format,var)
 #  define BD_ATTR_MALLOC
+#  define BD_ATTR_PACKED
 #endif
 
 #endif /* LIBBLURAY_ATTRIBUTES_H_ */
