25d1571f0b46d0a567ef576b1083bfb865774156
gates <gates@anonymous.org>
2010-05-28 16:11:02 +0000

    Add copyright and license information for code written by hpi1 hpi1 gave the go ahead in IRC to add license terms

--- src/examples/mobj_dump.c
+++ src/examples/mobj_dump.c
@@ -1,3 +1,31 @@
+/*
+ * This file is part of libbluray
+ * Copyright (C) 2010  hpi1
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * In addition, as a special exception, the copyright holders of libbluray
+ * gives permission to link the code of its release of libbluray with the
+ * OpenSSL project's "OpenSSL" library (or with modified versions of it
+ * that use the same license as the "OpenSSL" library), and distribute
+ * the linked executables.  You must obey the GNU General Public License
+ * in all respects for all of the code used other than "OpenSSL".  If you
+ * modify this file, you may extend this exception to your version of the
+ * file, but you are not obligated to do so.  If you do not wish to do
+ * so, delete this exception statement from your version.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
