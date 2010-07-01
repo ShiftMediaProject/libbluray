5427b1bb03ad1e296b684d84541d36b4904704ac
hpi1 <hpi1@anonymous.org>
2010-07-01 17:22:22 +0300

    Check if __GNUC__ is defined before using it

--- src/util/attributes.h
+++ src/util/attributes.h
@@ -20,7 +20,7 @@
 #ifndef LIBBLURAY_ATTRIBUTES_H_
 #define LIBBLURAY_ATTRIBUTES_H_
 
-#if __GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 3 )
+#if defined(__GNUC__) && (__GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 3 ))
 #    define BD_ATTR_FORMAT_PRINTF(format,var) __attribute__((__format__(__printf__,format,var)))
 #    define BD_ATTR_MALLOC                    __attribute__((__malloc__))
 #    define BD_ATTR_PACKED                    __attribute__((packed))
@@ -30,7 +30,7 @@
 #    define BD_ATTR_PACKED
 #endif
 
-#if __GNUC__ >= 4
+#if defined(__GNUC__) && __GNUC__ >= 4
 #    define BD_PUBLIC  __attribute__((visibility("default")))
 #    define BD_PRIVATE __attribute__((visibility("hidden")))
 #else
