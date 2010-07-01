2532faf6c934ca0fcd91b30bef0259e6fc8ba337
hpi1 <hpi1@anonymous.org>
2010-07-01 17:21:49 +0300

    Cosmetics

--- src/util/attributes.h
+++ src/util/attributes.h
@@ -21,21 +21,21 @@
 #define LIBBLURAY_ATTRIBUTES_H_
 
 #if __GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 3 )
-#  define BD_ATTR_FORMAT_PRINTF(format,var) __attribute__((__format__(__printf__,format,var)))
-#  define BD_ATTR_MALLOC                    __attribute__((__malloc__))
-#  define BD_ATTR_PACKED                    __attribute__((packed))
+#    define BD_ATTR_FORMAT_PRINTF(format,var) __attribute__((__format__(__printf__,format,var)))
+#    define BD_ATTR_MALLOC                    __attribute__((__malloc__))
+#    define BD_ATTR_PACKED                    __attribute__((packed))
 #else
-#  define BD_ATTR_FORMAT_PRINTF(format,var)
-#  define BD_ATTR_MALLOC
-#  define BD_ATTR_PACKED
+#    define BD_ATTR_FORMAT_PRINTF(format,var)
+#    define BD_ATTR_MALLOC
+#    define BD_ATTR_PACKED
 #endif
 
 #if __GNUC__ >= 4
-#  define BD_PUBLIC  __attribute__((visibility("default")))
-#  define BD_PRIVATE __attribute__((visibility("hidden")))
+#    define BD_PUBLIC  __attribute__((visibility("default")))
+#    define BD_PRIVATE __attribute__((visibility("hidden")))
 #else
-#  define BD_PUBLIC
-#  define BD_PRIVATE
+#    define BD_PUBLIC
+#    define BD_PRIVATE
 #endif
 
 #endif /* LIBBLURAY_ATTRIBUTES_H_ */
