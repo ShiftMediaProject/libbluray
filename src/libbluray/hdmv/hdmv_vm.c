80a1238585d59bfd75b99cb183067d01b85c6947
hpi1 <hpi1@anonymous.org>
2010-07-12 11:29:27 +0300

    Added SET/SETSYSTEM instructions

--- src/libbluray/hdmv/hdmv_vm.c
+++ src/libbluray/hdmv/hdmv_vm.c
@@ -29,8 +29,13 @@
 
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
+#include <sys/time.h>
 
+
+typedef struct {
+    struct timeval time;
+    uint32_t mobj_id;
+} NV_TIMER;
 
 struct hdmv_vm_s {
     /* state */
@@ -39,6 +44,8 @@ struct hdmv_vm_s {
     MOBJ_OBJECT   *object;        /* currently running object code */
 
     HDMV_EVENT     event[5];      /* pending events to return */
+
+    NV_TIMER       nv_timer;      /* navigation timer */
 
     /* movie objects */
     MOBJ_OBJECTS  *movie_objects; /* disc movie objects */
@@ -372,6 +379,207 @@ static int _play_stop(HDMV_VM *p)
 }
 
 /*
+ * SET/SYSTEM setstream
+ */
+
+static void _set_stream(HDMV_VM *p, uint32_t dst, uint32_t src)
+{
+    DEBUG(DBG_HDMV, "_set_stream(0x%x, 0x%x)\n", dst, src);
+
+    /* primary audio stream */
+    if (dst & 0x80000000) {
+        bd_psr_write(p->regs, PSR_PRIMARY_AUDIO_ID, (dst >> 16) & 0xfff);
+    }
+
+    /* IG stream */
+    if (src & 0x80000000) {
+        bd_psr_write(p->regs, PSR_IG_STREAM_ID, (src >> 16) & 0xff);
+    }
+
+    /* angle number */
+    if (src & 0x8000) {
+        bd_psr_write(p->regs, PSR_ANGLE_NUMBER, src & 0xff);
+    }
+
+    /* PSR2 */
+
+    uint32_t psr2 = bd_psr_read(p->regs, PSR_PG_PIP_STREAM);
+
+    /* PG TextST stream number */
+    if (dst & 0x8000) {
+        uint32_t text_st_num = dst & 0xfff;
+        psr2 = text_st_num | (psr2 & 0xfffff000);
+    }
+
+    /* Update PG TextST stream display flag */
+    uint32_t disp_s_flag = (dst & 0x4000) << 17;
+    psr2 = disp_s_flag | (psr2 & 0x7fffffff);
+
+    bd_psr_write(p->regs, PSR_PG_PIP_STREAM, psr2);
+}
+
+static void _set_sec_stream(HDMV_VM *p, uint32_t dst, uint32_t src)
+{
+    DEBUG(DBG_HDMV, "_set_sec_stream(0x%x, 0x%x)\n", dst, src);
+
+    uint32_t disp_v_flag   = (dst >> 30) & 1;
+    uint32_t disp_a_flag   = (src >> 30) & 1;
+    uint32_t text_st_flags = (src >> 13) & 3;
+
+    /* PSR14 */
+
+    uint32_t psr14 = bd_psr_read(p->regs, PSR_SECONDARY_AUDIO_VIDEO);
+
+    /* secondary video */
+    if (dst & 0x80000000) {
+      uint32_t sec_video = dst & 0xff;
+      psr14 = (sec_video << 8) | (psr14 & 0xffff00ff);
+    }
+
+    /* secondary video size */
+    if (dst & 0x00800000) {
+      uint32_t video_size = (dst >> 16) & 0xf;
+      psr14 = (video_size << 24) | (psr14 & 0xf0ffffff);
+    }
+
+    /* secondary audio */
+    if (src & 0x80000000) {
+      uint32_t sec_audio = (src >> 16) & 0xff;
+      psr14 = sec_audio | (psr14 & 0xffffff00);
+    }
+
+    psr14 = (disp_v_flag << 31) | (psr14 & 0x7fffffff);
+    psr14 = (disp_a_flag << 30) | (psr14 & 0xbfffffff);
+
+    bd_psr_write(p->regs, PSR_SECONDARY_AUDIO_VIDEO, psr14);
+
+    /* PSR2 */
+
+    uint32_t psr2  = bd_psr_read(p->regs, PSR_PG_PIP_STREAM);
+
+    /* PiP PG TextST stream */
+    if (src & 0x8000) {
+        uint32_t stream = src & 0xfff;
+        psr2 = (stream << 16) | (psr2 & 0xf000ffff);
+    }
+
+    psr2 = (text_st_flags << 30) | (psr2 & 0x3fffffff);
+
+    bd_psr_write(p->regs, PSR_PG_PIP_STREAM, psr2);
+}
+
+/*
+ * SET/SYSTEM navigation control
+ */
+
+static void _set_button_page(HDMV_VM *p, uint32_t dst, uint32_t src)
+{
+    if (p->ig_object) {
+        uint32_t current_page = bd_psr_read(p->regs, PSR_MENU_PAGE_ID) & 0xff;
+        if (src & 0x80000000 && (src & 0xff) == current_page) {
+            /* page does not change --> no effect (8.8.4.6.5.3.4) */
+            return;
+        }
+
+        // TODO - need to know valid page/button ids from IG stream
+
+        return;
+    }
+
+    /* selected button */
+    if (dst & 0x80000000) {
+        bd_psr_write(p->regs, PSR_SELECTED_BUTTON_ID, dst & 0xffff);
+    }
+
+    /* active page */
+    if (src & 0x80000000) {
+        bd_psr_write(p->regs, PSR_MENU_PAGE_ID, src & 0xff);
+    }
+}
+
+static void _enable_button(HDMV_VM *p, uint32_t dst, int enable)
+{
+    /* not valid in movie objects */
+    if (p->ig_object) {
+        if (enable) {
+            _queue_event(p, HDMV_EVENT_ENABLE_BUTTON,  dst);
+        } else {
+            _queue_event(p, HDMV_EVENT_DISABLE_BUTTON, dst);
+        }
+    }
+}
+
+static void _set_still_mode(HDMV_VM *p, int enable)
+{
+    /* not valid in movie objects */
+    if (p->ig_object) {
+        _queue_event(p, HDMV_EVENT_STILL, enable);
+    }
+}
+
+static void _popup_off(HDMV_VM *p)
+{
+    /* not valid in movie objects */
+    if (p->ig_object) {
+        _queue_event(p, HDMV_EVENT_POPUP_OFF, 1);
+    }
+}
+
+/*
+ * navigation timer
+ */
+
+static void _set_nv_timer(HDMV_VM *p, uint32_t dst, uint32_t src)
+{
+  uint32_t mobj_id = dst & 0xffff;
+  uint32_t timeout = src & 0xffff;
+
+  if (!timeout) {
+    /* cancel timer */
+    p->nv_timer.time.tv_sec = 0;
+    return;
+  }
+
+  /* validate params */
+  if (mobj_id >= p->movie_objects->num_objects) {
+      DEBUG(DBG_HDMV|DBG_CRIT, "_set_nv_timer(): invalid object id (%d) !\n", mobj_id);
+      return;
+  }
+  if (timeout > 300) {
+      DEBUG(DBG_HDMV|DBG_CRIT, "_set_nv_timer(): invalid timeout (%d) !\n", timeout);
+      return;
+  }
+
+  /* set expiration time */
+  gettimeofday(&p->nv_timer.time, NULL);
+  p->nv_timer.time.tv_sec += timeout;
+
+  p->nv_timer.mobj_id = mobj_id;
+}
+
+static int _check_nv_timer(HDMV_VM *p)
+{
+    if (p->nv_timer.time.tv_sec > 0) {
+        struct timeval now;
+        gettimeofday(&now, NULL);
+
+        if (now.tv_sec > p->nv_timer.time.tv_sec ||
+            ( now.tv_sec  == p->nv_timer.time.tv_sec &&
+              now.tv_usec >= p->nv_timer.time.tv_usec)) {
+
+            DEBUG(DBG_HDMV, "navigation timer expired, jumping to object %d\n", p->nv_timer.mobj_id);
+
+            p->nv_timer.time.tv_sec = 0;
+            _jump_object(p, p->nv_timer.mobj_id);
+
+            return 0;
+        }
+    }
+
+    return -1;
+}
+
+/*
  * trace
  */
 
@@ -567,6 +775,15 @@ static int _hdmv_step(HDMV_VM *p)
                 }
                 case SET_SETSYSTEM:
                     switch (insn->set_opt) {
+                        case INSN_SET_STREAM:      _set_stream     (p, dst, src); break;
+                        case INSN_SET_SEC_STREAM:  _set_sec_stream (p, dst, src); break;
+                        case INSN_SET_NV_TIMER:    _set_nv_timer   (p, dst, src); break;
+                        case INSN_SET_BUTTON_PAGE: _set_button_page(p, dst, src); break;
+                        case INSN_ENABLE_BUTTON:   _enable_button  (p, dst,   1); break;
+                        case INSN_DISABLE_BUTTON:  _enable_button  (p, dst,   0); break;
+                        case INSN_POPUP_OFF:       _popup_off      (p);           break;
+                        case INSN_STILL_ON:        _set_still_mode (p,   1);      break;
+                        case INSN_STILL_OFF:       _set_still_mode (p,   0);      break;
                         default:
                             DEBUG(DBG_HDMV|DBG_CRIT, "[unknown SETSYSTEM option in opcode 0x%08x] ", *(uint32_t*)insn);
                             break;
