5822a164c6b9765d3ec4c9270631ebcfa4fc8279
hpi1 <hpi1@anonymous.org>
2010-07-25 23:13:21 +0300

    Moved public part of logging.h to log_control.h

--- src/util/log_control.h
+++ src/util/log_control.h
@@ -18,20 +18,41 @@
  * <http://www.gnu.org/licenses/>.
  */
 
-#ifndef LOGGING_H_
-#define LOGGING_H_
-
-#include "log_control.h"
-
-#include "attributes.h"
+#ifndef BD_LOG_CONTROL_H_
+#define BD_LOG_CONTROL_H_
 
 #include <stdint.h>
 
-#define DEBUG(X,Y,...) bd_debug(__FILE__,__LINE__,X,Y,##__VA_ARGS__)
+
+enum debug_mask_enum {
+    DBG_RESERVED = 1,
+    DBG_CONFIGFILE = 2,
+    DBG_FILE = 4,
+    DBG_AACS = 8,
+    DBG_MKB = 16,
+    DBG_MMC = 32,
+    DBG_BLURAY = 64,
+    DBG_DIR = 128,
+    DBG_NAV = 256,
+    DBG_BDPLUS = 512,
+    DBG_DLX = 1024,
+    DBG_CRIT = 2048,         // this is libbluray's default debug mask so use this if you want to display critical info
+    DBG_HDMV = 4096,
+    DBG_BDJ = 8192,
+};
+
+typedef enum debug_mask_enum debug_mask_t;
+
+typedef void (*BD_LOG_FUNC)(const char *);
+
+/*
+ *
+ */
+
+void bd_set_debug_handler(BD_LOG_FUNC);
+
+void bd_set_debug_mask(debug_mask_t mask);
+debug_mask_t bd_get_debug_mask(void);
 
 
-BD_PRIVATE char *print_hex(char *out, const uint8_t *str, int count);
-BD_PRIVATE void bd_debug(const char *file, int line, uint32_t mask, const char *format, ...) BD_ATTR_FORMAT_PRINTF(4,5);
-
-
-#endif /* LOGGING_H_ */
+#endif /* BD_LOG_CONTROL_H_ */
