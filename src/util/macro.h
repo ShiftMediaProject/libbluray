2e9689dcc26f8af88815eef5293df4202123c675
hpi1 <hpi1@anonymous.org>
2010-08-20 11:25:22 +0300

    macro.h: include stdlib.h [ free() ]

--- src/util/macro.h
+++ src/util/macro.h
@@ -21,7 +21,8 @@
 #ifndef MACRO_H_
 #define MACRO_H_
 
-#include <stdio.h>
+#include <stdio.h>   /* fprintf() */
+#include <stdlib.h>  /* free() */
 
 #define HEX_PRINT(X,Y) { int zz; for(zz = 0; zz < Y; zz++) fprintf(stderr, "%02X", X[zz]); fprintf(stderr, "\n"); }
 #define MKINT_BE16(X) ( (X)[0] << 8 | (X)[1] )
