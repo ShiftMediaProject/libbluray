80374938ea57c6159724a8b1cfd48a71c1d1e411
hpi1 <hpi1@anonymous.org>
2010-09-05 15:50:32 +0300

    Added user input keys

---
+++ src/libbluray/keys.h
@@ -0,0 +1,53 @@
+/*
+ * This file is part of libbluray
+ * Copyright (C) 2010  hpi1
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#if !defined(_BD_KEYS_H_)
+#define _BD_KEYS_H_
+
+/*
+ * User input
+ */
+
+#define BD_VK_NONE       0xffff
+
+/* numeric key events */
+#define BD_VK_0          0
+#define BD_VK_1          1
+#define BD_VK_2          2
+#define BD_VK_3          3
+#define BD_VK_4          4
+#define BD_VK_5          5
+#define BD_VK_6          6
+#define BD_VK_7          7
+#define BD_VK_8          8
+#define BD_VK_9          9
+
+/* */
+#define BD_VK_ROOT_MENU  10  /* open root menu */
+#define BD_VK_POPUP      11  /* toggle popup menu */
+
+/* interactive key events */
+#define BD_VK_UP         12
+#define BD_VK_DOWN       13
+#define BD_VK_LEFT       14
+#define BD_VK_RIGHT      15
+#define BD_VK_ENTER      16
+
+
+#endif // _BD_KEYS_H_
