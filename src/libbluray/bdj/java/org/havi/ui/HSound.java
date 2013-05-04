c0e277e6839ce99534855206dcb97975564d5044
hpi1 <hpi1@anonymous.org>
2013-05-04 22:21:35 +0300

    Implement HSound

--- src/libbluray/bdj/java/org/havi/ui/HSound.java
+++ src/libbluray/bdj/java/org/havi/ui/HSound.java
@@ -1,6 +1,7 @@
 /*
  * This file is part of libbluray
  * Copyright (C) 2010  William Hahne
+ * Copyright (C) 2013  Petri Hintukainen <phintuka@users.sourceforge.net>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -22,21 +23,38 @@ package org.havi.ui;
 import java.io.IOException;
 import java.net.URL;
 
+import javax.media.ControllerEvent;
+import javax.media.ControllerListener;
+import javax.media.EndOfMediaEvent;
+import javax.media.Manager;
+import javax.media.NoPlayerException;
+import javax.media.Player;
+import javax.media.Time;
+
 import org.videolan.Logger;
 
 public class HSound {
+    boolean isLooping = false;
+    Player player = null;
+    ControllerListenerImpl listener;
+
     public HSound() {
-        logger.unimplemented("HSound");
+        listener = new ControllerListenerImpl();
     }
 
     public void load(String location) throws IOException, SecurityException {
-        logger.unimplemented("load");
-        logger.info("load("+location+")");
+        load(new URL("file:" + location));
     }
 
     public void load(URL contents) throws IOException, SecurityException {
-        logger.unimplemented("load");
-        logger.info("load("+contents+")");
+        dispose();
+        try {
+            player = Manager.createPlayer(contents);
+        } catch (NoPlayerException e) {
+            IOException ioe = new IOException();
+            ioe.initCause(e);
+            throw ioe;
+        }
     }
 
     public void set(byte data[]) {
@@ -44,19 +62,52 @@ public class HSound {
     }
 
     public void play() {
-        logger.unimplemented("play");
+        if (player != null) {
+            isLooping = false;
+            player.addControllerListener(listener);
+            player.start();
+        }
     }
 
     public void stop() {
-        logger.unimplemented("stop");
+        if (player != null) {
+            player.removeControllerListener(listener);
+            player.stop();
+        }
     }
 
     public void loop() {
-        //logger.unimplemented("loop");
+        if (player != null) {
+            isLooping = true;
+            player.removeControllerListener(listener);
+            player.start();
+        }
     }
 
     public void dispose() {
-        //logger.unimplemented("dispose");
+        if (player != null) {
+            player.removeControllerListener(listener);
+            player.stop();
+            player.close();
+            player = null;
+        }
+    }
+
+
+    private class ControllerListenerImpl implements ControllerListener {
+        private ControllerListenerImpl() {
+        }
+
+        public void controllerUpdate(ControllerEvent event) {
+            if (event instanceof EndOfMediaEvent) {
+                if (isLooping) {
+                    player.setMediaTime(new Time(0L));
+                    player.start();
+                } else {
+                    stop();
+                }
+            }
+        }
     }
 
     private static final Logger logger = Logger.getLogger(HSound.class.getName());
