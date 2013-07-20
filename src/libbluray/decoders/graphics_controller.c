67b87980e4b0afc0d653215895d07a372c075dc6
hpi1 <hpi1@anonymous.org>
2013-07-20 13:45:06 +0300

    Fixed printf format

--- src/libbluray/decoders/graphics_controller.c
+++ src/libbluray/decoders/graphics_controller.c
@@ -964,7 +964,7 @@ static int _render_textst(GRAPHICS_CONTROLLER *p, uint32_t stc, GC_NAV_CMDS *cmd
         /* next dialog too far in future ? */
         if (dialog[ii].start_pts >= now + 90000) {
             cmds->wakeup_time = dialog[ii].start_pts / 2;
-            GC_TRACE("_render_textst(): next event #%d in %lld seconds (pts %"PRId64")\n",
+            GC_TRACE("_render_textst(): next event #%d in %"PRId64" seconds (pts %"PRId64")\n",
                      ii, (dialog[ii].start_pts - now)/90000, dialog[ii].start_pts);
             return 1;
         }
