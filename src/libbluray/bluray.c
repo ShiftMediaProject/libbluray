86227e2860c871e0a2e8d287b75559df8ccc0567
Sean McGovern <gseanmcg@gmail.com>
2013-09-11 23:22:17 -0400

    rename a variable as it shadows a global definition

--- src/libbluray/bluray.c
+++ src/libbluray/bluray.c
@@ -482,9 +482,9 @@ static void _update_textst_timer(BLURAY *bd)
 static void _init_textst_timer(BLURAY *bd)
 {
     if (bd->st_textst.clip) {
-        uint32_t time;
-        clpi_access_point(bd->st0.clip->cl, bd->st0.clip_block_pos/192, /*next=*/0, /*angle_change=*/0, &time);
-        bd->gc_wakeup_time = time;
+        uint32_t clip_time;
+        clpi_access_point(bd->st0.clip->cl, bd->st0.clip_block_pos/192, /*next=*/0, /*angle_change=*/0, &clip_time);
+        bd->gc_wakeup_time = clip_time;
         bd->gc_wakeup_pos = 0;
         _update_textst_timer(bd);
     }
