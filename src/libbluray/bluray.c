6445e17a8a4e3579369e8e2425a53ab9603c3ed3
hpi1 <hpi1@anonymous.org>
2013-11-12 09:56:00 +0200

    Fixed ARGB overlay without application buffer

--- src/libbluray/bluray.c
+++ src/libbluray/bluray.c
@@ -1015,7 +1015,7 @@ static void _bdj_osd_cb(BLURAY *bd, const unsigned *img, int w, int h,
     }
 
     /* pass only changed region */
-    if (bd->argb_buffer->width < w || bd->argb_buffer->height < h) {
+    if (bd->argb_buffer && (bd->argb_buffer->width < w || bd->argb_buffer->height < h)) {
         aov.argb   = img;
     } else {
         aov.argb   = img + x0 + y0 * w;
