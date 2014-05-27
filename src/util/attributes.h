e2e355ab62fe25ce42080423fdafa9e9662d04d7
Jean-Baptiste Kempf <jb@videolan.org>
2014-05-27 11:49:51 +0200

    Use GNU/Mingw printf for Mingw builds
    This should kill most warnings when crosscompiling for Windows

--- src/util/attributes.h
+++ src/util/attributes.h
@@ -21,7 +21,11 @@
 #define LIBBLURAY_ATTRIBUTES_H_
 
 #if defined(__GNUC__) && (__GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 3 ))
-#    define BD_ATTR_FORMAT_PRINTF(format,var) __attribute__((__format__(__printf__,format,var)))
+#    if defined(_WIN32)
+#        define BD_ATTR_FORMAT_PRINTF(format,var) __attribute__((__format__(__gnu_printf__,format,var)))
+#    else
+#        define BD_ATTR_FORMAT_PRINTF(format,var) __attribute__((__format__(__printf__,format,var)))
+#    endif
 #    define BD_ATTR_MALLOC                    __attribute__((__malloc__))
 #    define BD_ATTR_PACKED                    __attribute__((packed))
 #else
