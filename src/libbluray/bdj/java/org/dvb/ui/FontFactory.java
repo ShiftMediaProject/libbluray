ead64a5d1ad173c68c6f0f2a6eec605bfe8963f8
hpi1 <hpi1@anonymous.org>
2014-11-23 14:22:08 +0200

    No need to use separate org.dvb.ui.FontFactory for SE and ME

--- src/libbluray/bdj/java/org/dvb/ui/FontFactory.java
+++ src/libbluray/bdj/java/org/dvb/ui/FontFactory.java
@@ -1,6 +1,7 @@
 /*
  * This file is part of libbluray
  * Copyright (C) 2010  William Hahne
+ * Copyright (C) 2014  Petri Hintukainen <phintuka@users.sourceforge.net>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
