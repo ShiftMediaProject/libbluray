73ce0c81af7de25a5c9c4cd5c367eeacea33dfe1
hpi1 <hpi1@anonymous.org>
2015-09-14 13:52:36 +0300

    Avoid returning null for content certificate ID

--- src/libbluray/bdj/java/com/aacsla/bluray/online/ContentAttribute.java
+++ src/libbluray/bdj/java/com/aacsla/bluray/online/ContentAttribute.java
@@ -27,10 +27,19 @@ public class ContentAttribute {
     }
 
     public byte[] getContentCertID() {
+        byte[] id = getContentCertID("AACS" + File.separator + "Content000.cer");
+        if (id != null) {
+            return id;
+        }
+
+        return new byte[6];
+    }
+
+    private byte[] getContentCertID(String file) {
         FileInputStream is = null;
         try {
             is = new FileInputStream(
-                System.getProperty("bluray.vfs.root") + File.separator + "AACS/Content000.cer");
+                System.getProperty("bluray.vfs.root") + File.separator + file);
             if (is.skip(14) != 14)
                 return null;
             byte[] bytes = new byte[6];
