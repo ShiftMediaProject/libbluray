2708e41e06bfc03199733faa4c5b133807d9336f
hpi1 <hpi1@anonymous.org>
2016-03-23 12:18:13 +0200

    BDJThreadGroup: make sure destroy() is not called if group has been already destroyed
    Not fatal, but prevents incorrect alarms in log.

--- src/libbluray/bdj/java/org/videolan/BDJThreadGroup.java
+++ src/libbluray/bdj/java/org/videolan/BDJThreadGroup.java
@@ -85,8 +85,11 @@ class BDJThreadGroup extends ThreadGroup {
             waitForShutdown(500, 0);
         }
 
+        if (destroyed) return;
+
         try {
             destroy();
+            destroyed = true;
         } catch (IllegalThreadStateException e) {
             logger.error("ThreadGroup destroy failed: " + e);
         }
@@ -109,6 +112,7 @@ class BDJThreadGroup extends ThreadGroup {
         }
     }
 
+    private boolean destroyed = false;
     private final BDJXletContext context;
     private static final Logger logger = Logger.getLogger(BDJThreadGroup.class.getName());
 }
