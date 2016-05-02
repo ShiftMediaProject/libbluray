4c2bd40efe8e574e79d25761151ec243e70a27cc
hpi1 <hpi1@anonymous.org>
2016-05-02 11:35:03 +0300

    BDHandler: log unimplemented getTargetState()

--- src/libbluray/bdj/java/org/videolan/media/content/BDHandler.java
+++ src/libbluray/bdj/java/org/videolan/media/content/BDHandler.java
@@ -104,6 +104,7 @@ public abstract class BDHandler implements Player, ServiceContentHandler {
     }
 
     public int getTargetState() {
+        Logger.unimplemented("BDHandler", "getTargetState()");
         synchronized (this) {
             return targetState;
         }
