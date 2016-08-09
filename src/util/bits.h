2921e008c9ed061f84379c5913f3c29ec01109bc
hpi1 <hpi1@anonymous.org>
2016-08-09 15:11:00 +0300

    bits.h: add BD_USED

--- src/util/bits.h
+++ src/util/bits.h
@@ -55,11 +55,11 @@ typedef struct {
 } BITSTREAM;
 
 BD_PRIVATE void bb_init( BITBUFFER *bb, const uint8_t *p_data, size_t i_data );
-BD_PRIVATE int  bs_init( BITSTREAM *bs, BD_FILE_H *fp );
+BD_PRIVATE int  bs_init( BITSTREAM *bs, BD_FILE_H *fp ) BD_USED;
 BD_PRIVATE void bb_seek( BITBUFFER *bb, int64_t off, int whence);
 //BD_PRIVATE void bs_seek( BITSTREAM *bs, int64_t off, int whence);
 //BD_PRIVATE void bb_seek_byte( BITBUFFER *bb, int64_t off);
-BD_PRIVATE int  bs_seek_byte( BITSTREAM *s, int64_t off);
+BD_PRIVATE int  bs_seek_byte( BITSTREAM *s, int64_t off) BD_USED;
 BD_PRIVATE void bb_skip( BITBUFFER *bb, size_t i_count );
 BD_PRIVATE void bs_skip( BITSTREAM *bs, size_t i_count );  /* note: i_count must be less than BF_BUF_SIZE */
 BD_PRIVATE uint32_t bb_read( BITBUFFER *bb, int i_count );
