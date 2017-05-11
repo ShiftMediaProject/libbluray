abdf7b153cce1c0c1ec7f47220fe85c981193c5f
hpi1 <hpi1@anonymous.org>
2017-05-11 17:29:34 +0300

    Cosmetics

--- src/libbluray/bluray.c
+++ src/libbluray/bluray.c
@@ -2810,21 +2810,21 @@ int bd_set_player_setting_str(BLURAY *bd, uint32_t idx, const char *s)
         case BLURAY_PLAYER_SETTING_COUNTRY_CODE:
             return bd_set_player_setting(bd, idx, str_to_uint32(s, 2));
 
-                case BLURAY_PLAYER_CACHE_ROOT:
-                    bd_mutex_lock(&bd->mutex);
-                    X_FREE(bd->bdjstorage.cache_root);
-                    bd->bdjstorage.cache_root = str_dup(s);
-                    bd_mutex_unlock(&bd->mutex);
-                    BD_DEBUG(DBG_BDJ, "Cache root dir set to %s\n", bd->bdjstorage.cache_root);
-                    return 1;
+        case BLURAY_PLAYER_CACHE_ROOT:
+            bd_mutex_lock(&bd->mutex);
+            X_FREE(bd->bdjstorage.cache_root);
+            bd->bdjstorage.cache_root = str_dup(s);
+            bd_mutex_unlock(&bd->mutex);
+            BD_DEBUG(DBG_BDJ, "Cache root dir set to %s\n", bd->bdjstorage.cache_root);
+            return 1;
 
-                case BLURAY_PLAYER_PERSISTENT_ROOT:
-                    bd_mutex_lock(&bd->mutex);
-                    X_FREE(bd->bdjstorage.persistent_root);
-                    bd->bdjstorage.persistent_root = str_dup(s);
-                    bd_mutex_unlock(&bd->mutex);
-                    BD_DEBUG(DBG_BDJ, "Persistent root dir set to %s\n", bd->bdjstorage.persistent_root);
-                    return 1;
+        case BLURAY_PLAYER_PERSISTENT_ROOT:
+            bd_mutex_lock(&bd->mutex);
+            X_FREE(bd->bdjstorage.persistent_root);
+            bd->bdjstorage.persistent_root = str_dup(s);
+            bd_mutex_unlock(&bd->mutex);
+            BD_DEBUG(DBG_BDJ, "Persistent root dir set to %s\n", bd->bdjstorage.persistent_root);
+            return 1;
 
         default:
             return 0;
