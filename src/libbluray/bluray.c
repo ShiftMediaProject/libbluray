6e63d7785b1ff8c9a9866b0acf892a4d1dd8090a
Petri Hintukainen <phintuka@gmail.com>
2022-10-03 19:47:16 +0300

    Protect title_list access with mutex
    It doesn't make much sense to call hese functions in paraller,
    but doing so might cause some harm ...

--- src/libbluray/bluray.c
+++ src/libbluray/bluray.c
@@ -2543,10 +2543,9 @@ int bd_play_playlist_at(BLURAY *bd, int playlist, int playitem, int playmark, in
 // Select a title for playback
 // The title index is an index into the list
 // established by bd_get_titles()
-int bd_select_title(BLURAY *bd, uint32_t title_idx)
+static int _select_title(BLURAY *bd, uint32_t title_idx)
 {
     const char *f_name;
-    int result;
 
     // Open the playlist
     if (bd->title_list == NULL) {
@@ -2558,13 +2557,18 @@ int bd_select_title(BLURAY *bd, uint32_t title_idx)
         return 0;
     }
 
-    bd_mutex_lock(&bd->mutex);
-
     bd->title_idx = title_idx;
     f_name = bd->title_list->title_info[title_idx].name;
 
-    result = _open_playlist(bd, f_name, 0);
+    return _open_playlist(bd, f_name, 0);
+}
 
+int bd_select_title(BLURAY *bd, uint32_t title_idx)
+{
+    int result;
+
+    bd_mutex_lock(&bd->mutex);
+    result = _select_title(bd, title_idx);
     bd_mutex_unlock(&bd->mutex);
 
     return result;
@@ -2646,38 +2650,55 @@ void bd_seamless_angle_change(BLURAY *bd, unsigned angle)
 
 uint32_t bd_get_titles(BLURAY *bd, uint8_t flags, uint32_t min_title_length)
 {
+    NAV_TITLE_LIST *title_list;
+    uint32_t count;
+
     if (!bd) {
         return 0;
     }
 
-    nav_free_title_list(&bd->title_list);
-    bd->title_list = nav_get_title_list(bd->disc, flags, min_title_length);
-
-    if (!bd->title_list) {
+    title_list = nav_get_title_list(bd->disc, flags, min_title_length);
+    if (!title_list) {
         BD_DEBUG(DBG_BLURAY | DBG_CRIT, "nav_get_title_list(%s) failed\n", disc_root(bd->disc));
         return 0;
     }
 
-    disc_event(bd->disc, DISC_EVENT_START, bd->disc_info.num_titles);
+    bd_mutex_lock(&bd->mutex);
 
-    return bd->title_list->count;
+    nav_free_title_list(&bd->title_list);
+    bd->title_list = title_list;
+
+    disc_event(bd->disc, DISC_EVENT_START, bd->disc_info.num_titles);
+    count = bd->title_list->count;
+
+    bd_mutex_unlock(&bd->mutex);
+
+    return count;
 }
 
 int bd_get_main_title(BLURAY *bd)
 {
+    int main_title_idx = -1;
+
     if (!bd) {
         return -1;
     }
+
+    bd_mutex_lock(&bd->mutex);
+
     if (bd->title_type != title_undef) {
         BD_DEBUG(DBG_CRIT | DBG_BLURAY, "bd_get_main_title() can't be used with BluRay menus\n");
     }
 
     if (bd->title_list == NULL) {
         BD_DEBUG(DBG_BLURAY | DBG_CRIT, "Title list not yet read!\n");
-        return -1;
+    } else {
+        main_title_idx = bd->title_list->main_title_idx;
     }
 
-    return bd->title_list->main_title_idx;
+    bd_mutex_unlock(&bd->mutex);
+
+    return main_title_idx;
 }
 
 static int _copy_streams(const NAV_CLIP *clip, BLURAY_STREAM_INFO **pstreams,
@@ -2828,19 +2849,26 @@ static BLURAY_TITLE_INFO *_get_title_info(BLURAY *bd, uint32_t title_idx, uint32
 
 BLURAY_TITLE_INFO* bd_get_title_info(BLURAY *bd, uint32_t title_idx, unsigned angle)
 {
+    char mpls_name[11] = "";
+    int  mpls_id = -1;
+
+    bd_mutex_lock(&bd->mutex);
+
     if (bd->title_list == NULL) {
         BD_DEBUG(DBG_BLURAY | DBG_CRIT, "Title list not yet read!\n");
-        return NULL;
-    }
-    if (bd->title_list->count <= title_idx) {
+    } else if (bd->title_list->count <= title_idx) {
         BD_DEBUG(DBG_BLURAY | DBG_CRIT, "Invalid title index %d!\n", title_idx);
-        return NULL;
+    } else {
+        mpls_id = bd->title_list->title_info[title_idx].mpls_id;
+        memcpy(mpls_name, bd->title_list->title_info[title_idx].name, 11);
     }
 
-    return _get_title_info(bd,
-                           title_idx, bd->title_list->title_info[title_idx].mpls_id,
-                           bd->title_list->title_info[title_idx].name,
-                           angle);
+    bd_mutex_unlock(&bd->mutex);
+
+    if (mpls_id < 0)
+        return NULL;
+
+    return _get_title_info(bd, title_idx, mpls_id, mpls_name, angle);
 }
 
 BLURAY_TITLE_INFO* bd_get_playlist_info(BLURAY *bd, uint32_t playlist, unsigned angle)
